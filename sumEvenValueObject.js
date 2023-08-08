2ee459ad782efd61a942d14db8dd36959b625675
Chandra Muhamad Apriana <aprianachandra@gmail.com>
2023-08-08 23:55:10 +0700

    fix: remove log

--- sumEvenValueObject.js
+++ sumEvenValueObject.js
@@ -15,13 +15,3 @@ const sumEvenValueObject = (object) => {
 };
 
 module.exports = sumEvenValueObject;
-
-console.log(
-  sumEvenValueObject({
-    a: 2,
-    b: { b: 2, bb: { b: 3, bb: { b: 2 } } },
-    c: { c: { c: 2 }, cc: "ball", ccc: 5 },
-    d: 1,
-    e: { e: { e: 4 }, ee: "car" },
-  })
-);
