55d78695fc018d446754d2fffbc47b2719e79a63
Chandra Muhamad Apriana <aprianachandra@gmail.com>
2023-08-10 19:45:52 +0700

    fix: add test case when the input is not an object or the object is empty

--- sumEvenValueObject.js
+++ sumEvenValueObject.js
@@ -1,5 +1,8 @@
 const sumEvenValueObject = (object) => {
+  if (typeof object !== "object") return "Input is not object";
   const keys = Object.keys(object);
+  if (keys.length === 0) return "Object is empty";
+
   let value = 0;
   for (let i = 0; i < keys.length; i++) {
     const valueObject = object[keys[i]];
